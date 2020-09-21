2e4884fcb380f259eebe348036b49d6f38016bf4
adityasharmaa <33754095+adityasharmaa@users.noreply.github.com>
2020-09-21 11:03:12 +0530

    Delete assets.dart

--- lib/config/assets.dart
+++
@@ -1,8 +0,0 @@
-class Assets{
-  static const images_base = "assets/images/";
-  static const cards_base = images_base + "cards/";
-  static const cards_image_login_screen = images_base + "cards_image_login_screen.png";
-  static const background = images_base + "background.jpg";
-  static const coin = images_base + "coin.png";
-  static const card_background = images_base + "cards_background.jpg";
-}
